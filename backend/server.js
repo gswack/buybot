cd13584f070b4d1e7fdca8801757f65914f78d53
swack.gabe@gmail.com <swack.gabe@gmail.com>
2025-11-03 22:11:41 +0200

    Added an environment file for backend folderm, containing token info

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+require("dotenv").config();
+console.log("Loaded token:", process.env.PRICEAPI_TOKEN);
+
 const express = require("express");
 const cors = require("cors");
 const searchRoutes = require("./routes/search");
@@ -7,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", searchRoutes);
 
-// const PORT = process.env.PORT || 5000;
-const PORT = 5e3;
+const PORT = process.env.PORT || 3000;
+// const PORT = 5e3;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
